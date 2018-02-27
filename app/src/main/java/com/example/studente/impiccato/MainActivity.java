8a062c60e19d69cd6ee5ec0c0cc775e8f4534588
Elena <elenabasei99@gmail.com>
2018-02-27 12:44:48 +0100

    grafica

--- app/src/main/java/com/example/studente/impiccato/MainActivity.java
+++ app/src/main/java/com/example/studente/impiccato/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.studente.impiccato;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton buttonGoTo = findViewById(R.id.btnGioca);
+        buttonGoTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GiocoActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
