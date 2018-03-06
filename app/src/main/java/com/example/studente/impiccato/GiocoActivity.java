8918f79246efbf9ba6df6c3b555de8b37be587b5
Elena <elenabasei99@gmail.com>
2018-03-06 12:48:38 +0100

    parole

--- app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
+++ app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
@@ -2,6 +2,16 @@ package com.example.studente.impiccato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GiocoActivity extends AppCompatActivity {
 
@@ -10,6 +20,33 @@ public class GiocoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gioco);
 
+        String json = null;
+        try {
+
+            InputStream is = this.getAssets().open("parole.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        ArrayList<String> parole=new ArrayList<>();
+
+        try {
+            JSONObject obj = new JSONObject(json);
+            JSONArray m_jArry = obj.getJSONArray("parole");
+
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                parole.add(jo_inside.getString("parola"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
