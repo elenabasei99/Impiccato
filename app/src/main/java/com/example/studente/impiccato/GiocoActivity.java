c50ebee9123131e393a7ef1835d5ddc10874d23e
Elena <elenabasei99@gmail.com>
2018-03-07 12:57:11 +0100

    bestemmiehghhhhhhhhhh

--- app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
+++ app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
@@ -3,6 +3,8 @@ package com.example.studente.impiccato;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class GiocoActivity extends AppCompatActivity {
 
@@ -48,5 +51,36 @@ public class GiocoActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        Random r = new Random();
+        int i = r.nextInt(1160 - 0) + 0;
+
+        TextView text = (TextView) findViewById(R.id.lettereU);
+        text.setText(parole.get(i));
+
+        ArrayList<Character> lettere = new ArrayList<>();
+
+        boolean flag=true;
+        int j=0;
+
+        while(flag){
+            try{
+                lettere.add((parole.get(i).charAt(j)));
+                j++;
+            }
+            catch (Exception e){
+                flag=false;
+            }
+        }
+
+        LinearLayout layout1= (LinearLayout) findViewById(R.id.layout1);
+
+        for(int index=0; index<lettere.size();index++){
+            TextView t=new TextView(null);
+            t.setText(""+lettere.get(index));
+            t.setWidth(40);
+            t.setHeight(0);
+            t.setId(index);
+            layout1.addView(t);
+        }
     }
 }
