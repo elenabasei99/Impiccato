884b4669a3f6c73dfa9940fcb6d19a2914d1b95d
denisbasei <denis@baseiengineering.com>
2018-03-09 19:30:03 +0100

    prova gioco

--- app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
+++ app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
@@ -4,6 +4,8 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,6 +21,8 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class GiocoActivity extends AppCompatActivity {
+    private ArrayList<Character> lettere = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +60,6 @@ public class GiocoActivity extends AppCompatActivity {
         Random r = new Random();
         int i = r.nextInt(1160 - 0) + 0;
 
-        TextView text = (TextView) findViewById(R.id.lettereU);
-        text.setText(parole.get(i));
-
-        ArrayList<Character> lettere = new ArrayList<>();
 
         boolean flag=true;
         int j=0;
@@ -67,7 +67,6 @@ public class GiocoActivity extends AppCompatActivity {
         while(flag){
             try{
                 lettere.add((parole.get(i).charAt(j)));
-                text.setText(""+text.getText()+lettere.get(j));
                 j++;
             }
             catch (Exception e){
@@ -81,18 +80,52 @@ public class GiocoActivity extends AppCompatActivity {
 
         for(int index=0; index<lettere.size();index++){
             TextView t=new TextView(GiocoActivity.this);
-            t.setText(""+lettere.get(index));
+
+            if(index==0){
+                t.setText(""+lettere.get(index));
+            }
+            else if(index==lettere.size()-1){
+                t.setText(""+lettere.get(index));
+            }
+            else{
+                t.setText("_");
+            }
+
             t.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.MATCH_PARENT));
             t.setWidth(90);
             t.setId(index);
+            t.setTextSize(22);
+            t.setAllCaps(true);
 
-
-            GradientDrawable gd = new GradientDrawable();
-            gd.setColor(0xFFFFFFFF); // Changes this drawbale to use a single color instead of a gradient
-            gd.setCornerRadius(5);
-            gd.setStroke(1, 0xFF000000);
-            t.setBackground(gd);
             layout1.addView(t);
         }
+
+        for(int index=1; index<lettere.size()-1;index++){
+            if(lettere.get(index).equals(lettere.get(0))){
+                TextView t= (TextView)findViewById(index);
+                t.setText(""+lettere.get(0));
+            }
+            else if(lettere.get(index).equals(lettere.get(lettere.size()-1))){
+                TextView t= (TextView)findViewById(index);
+                t.setText(""+lettere.get(lettere.size()-1));
+            }
+        }
+
+    }
+
+    public void lettere(View view) {
+        TextView t= (TextView)findViewById(R.id.lettereU);
+        Button b= (Button) findViewById(view.getId());
+        t.setText(t.getText()+"   "+b.getText());
+
+        for(int index=1; index<lettere.size()-1;index++){
+            if(lettere.get(index).equals(""+b.getText())){
+                TextView t1= (TextView)findViewById(index);
+                t.setText(""+b.getText());
+            }
+        }
+
+        LinearLayout layout2= (LinearLayout) findViewById(R.id.layout2);
+        layout2.removeView(view);
     }
 }
