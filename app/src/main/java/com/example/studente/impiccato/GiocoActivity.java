a978a97d30cddb6c1d434e157aecc898f3f2844f
denisbasei <denis@baseiengineering.com>
2018-03-12 19:25:37 +0100

    ultime modifiche

--- app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
+++ app/src/main/java/com/example/studente/impiccato/GiocoActivity.java
@@ -1,5 +1,7 @@
 package com.example.studente.impiccato;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -26,8 +28,10 @@ import java.util.Random;
 
 public class GiocoActivity extends AppCompatActivity {
     private ArrayList<Character> lettere = new ArrayList<>(),lettereU=new ArrayList<>();
-    String parola="",lettereu="";
-    private int lett=0,sbagli=1;
+    private String parola="",lettereu="";
+    private int lett=0,sbagli=1,numParole=1;
+    private Boolean prossima=false,risultato=false;
+    private Bundle savedInstanceState;
 
 
     @Override
@@ -35,219 +39,258 @@ public class GiocoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gioco);
 
-        if(savedInstanceState!=null){
-            lett=savedInstanceState.getInt("lett");
-            sbagli=savedInstanceState.getInt("sbagli");
-            parola=savedInstanceState.getString("parola");
-            lettereu=savedInstanceState.getString("lettereU");
+        this.savedInstanceState=savedInstanceState;
 
+        Intent intent= getIntent();
+        numParole=intent.getIntExtra("numParole",1);
 
-            boolean flag = true;
-            int j = 0;
+        try{
+            risultato=savedInstanceState.getBoolean("risultato");
+        }
+        catch (Exception e){
+            risultato=false;
+        }
 
-            while (flag) {
-                try {
-                    lettere.add((parola.charAt(j)));
-                    j++;
-                } catch (Exception e) {
-                    flag = false;
-                }
-            }
-
-            flag=true;
-            j=0;
-            while (flag) {
-                try {
-                    lettereU.add((lettereu.charAt(j)));
-                    j++;
-                } catch (Exception e) {
-                    flag = false;
-                }
-            }
-
-            int cont = 0;
-
-            LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
-            for (int index = 0; index < 26 - cont; index++) {
-                Button b = (Button) layout2.getChildAt(index);
-                if (("" + lettere.get(0)).equals("" + b.getText())) {
-                    layout2.removeViewAt(index);
-                    index--;
-                    cont++;
-                } else if (!("" + lettere.get(0)).equals(("" + lettere.get(lettere.size() - 1)))) {
-                    if (("" + lettere.get(lettere.size() - 1)).equals("" + b.getText())) {
-                        layout2.removeViewAt(index);
-                        index--;
-                        cont++;
-                    }
-                }
-            }
-
-
-            LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
-
-
-            for (int index = 0; index < lettere.size(); index++) {
-                TextView t = new TextView(GiocoActivity.this);
-
-                if (index == 0) {
-                    t.setText("" + lettere.get(index));
-                } else if (index == lettere.size() - 1) {
-                    t.setText("" + lettere.get(index));
-                } else {
-                    t.setText("_");
-                }
-
-                t.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT));
-                t.setWidth(90);
-                t.setId(index);
-                t.setTextSize(22);
-                t.setAllCaps(true);
-
-                layout1.addView(t);
-            }
-
-            for (int index = 1; index < lettere.size() - 1; index++) {
-                if (lettere.get(index).equals(lettere.get(0))) {
-                    TextView t = (TextView) findViewById(index);
-                    t.setText("" + lettere.get(0));
-                } else if (lettere.get(index).equals(lettere.get(lettere.size() - 1))) {
-                    TextView t = (TextView) findViewById(index);
-                    t.setText("" + lettere.get(lettere.size() - 1));
-                }
-            }
-
-            for(int ind=0;ind<lettereU.size();ind++){
-                TextView t= (TextView)findViewById(R.id.lettereU);
-                t.setText(t.getText()+"   "+lettereU.get(ind));
-
-                for(int index=1; index<lettere.size()-1;index++){
-
-                    if((""+lettere.get(index)).equals(""+lettereU.get(ind))){
-                        layout1= (LinearLayout)findViewById(R.id.layout1);
-                        TextView t1= (TextView)layout1.getChildAt(index);
-                        t1.setText(""+lettereU.get(ind));
-                    }
-                }
-
-                for (int index = 0; index < 26 - cont; index++) {
-                    Button b = (Button) layout2.getChildAt(index);
-                    if ((""+lettereU.get(ind)).equals("" + b.getText())) {
-                        layout2.removeViewAt(index);
-                        index--;
-                        cont++;
-                    }
-                }
-            }
-
-            ImageView img=(ImageView)findViewById(R.id.imageView);
-            img.setBackgroundResource(getResources().getIdentifier("imp"+sbagli, "drawable",  getPackageName()));
-
-
-
-
+        if(risultato){
+            setContentView(R.layout.activity_risultato);
         }
         else {
+            inizia();
+        }
+    }
 
-            String json = null;
+    private void inizia(){
+
+        try{
+            TextView titolo= (TextView)findViewById(R.id.titolo);
+            titolo.setText("Parola numero "+savedInstanceState.getInt("numParole"));
+            prossima=savedInstanceState.getBoolean("prossima");
+        }
+        catch (Exception e){
+
+        }
+
+        TextView titolo= (TextView)findViewById(R.id.titolo);
+        titolo.setText("Parola numero "+numParole);
+
+        if(savedInstanceState!=null && !prossima){
+            inCorso();
+        }
+        else {
+            prossima=false;
+            nuovo();
+        }
+    }
+
+    private void inCorso(){
+        lett=savedInstanceState.getInt("lett");
+        sbagli=savedInstanceState.getInt("sbagli");
+        parola=savedInstanceState.getString("parola");
+        lettereu=savedInstanceState.getString("lettereU");
+
+
+        boolean flag = true;
+        int j = 0;
+
+        while (flag) {
             try {
-
-                InputStream is = this.getAssets().open("parole.json");
-                int size = is.available();
-                byte[] buffer = new byte[size];
-                is.read(buffer);
-                is.close();
-                json = new String(buffer, "UTF-8");
-            } catch (IOException ex) {
-                ex.printStackTrace();
+                lettere.add((parola.charAt(j)));
+                j++;
+            } catch (Exception e) {
+                flag = false;
             }
+        }
 
-            ArrayList<String> parole = new ArrayList<>();
-
+        flag=true;
+        j=0;
+        while (flag) {
             try {
-                JSONObject obj = new JSONObject(json);
-                JSONArray m_jArry = obj.getJSONArray("parole");
-
-                for (int i = 0; i < m_jArry.length(); i++) {
-                    JSONObject jo_inside = m_jArry.getJSONObject(i);
-                    parole.add(jo_inside.getString("parola"));
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+                lettereU.add((lettereu.charAt(j)));
+                j++;
+            } catch (Exception e) {
+                flag = false;
             }
+        }
 
-            Random r = new Random();
-            int i = r.nextInt(1160 - 0) + 0;
+        int cont = 0;
 
-            parola = parole.get(i);
-
-            boolean flag = true;
-            int j = 0;
-
-            while (flag) {
-                try {
-                    lettere.add((parole.get(i).charAt(j)));
-                    j++;
-                } catch (Exception e) {
-                    flag = false;
-                }
-            }
-
-            int cont = 0;
-
-            LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
-            for (int index = 0; index < 26 - cont; index++) {
-                Button b = (Button) layout2.getChildAt(index);
-                if (("" + lettere.get(0)).equals("" + b.getText())) {
+        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
+        for (int index = 0; index < 26 - cont; index++) {
+            Button b = (Button) layout2.getChildAt(index);
+            if (("" + lettere.get(0)).equals("" + b.getText())) {
+                layout2.removeViewAt(index);
+                index--;
+                cont++;
+            } else if (!("" + lettere.get(0)).equals(("" + lettere.get(lettere.size() - 1)))) {
+                if (("" + lettere.get(lettere.size() - 1)).equals("" + b.getText())) {
                     layout2.removeViewAt(index);
                     index--;
                     cont++;
-                } else if (!("" + lettere.get(0)).equals(("" + lettere.get(lettere.size() - 1)))) {
-                    if (("" + lettere.get(lettere.size() - 1)).equals("" + b.getText())) {
-                        layout2.removeViewAt(index);
-                        index--;
-                        cont++;
-                    }
-                }
-            }
-
-
-            LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
-
-
-            for (int index = 0; index < lettere.size(); index++) {
-                TextView t = new TextView(GiocoActivity.this);
-
-                if (index == 0) {
-                    t.setText("" + lettere.get(index));
-                } else if (index == lettere.size() - 1) {
-                    t.setText("" + lettere.get(index));
-                } else {
-                    t.setText("_");
-                    lett++;
-                }
-
-                t.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT));
-                t.setWidth(90);
-                t.setId(index);
-                t.setTextSize(22);
-                t.setAllCaps(true);
-
-                layout1.addView(t);
-            }
-
-            for (int index = 1; index < lettere.size() - 1; index++) {
-                if (lettere.get(index).equals(lettere.get(0))) {
-                    TextView t = (TextView) findViewById(index);
-                    t.setText("" + lettere.get(0));
-                } else if (lettere.get(index).equals(lettere.get(lettere.size() - 1))) {
-                    TextView t = (TextView) findViewById(index);
-                    t.setText("" + lettere.get(lettere.size() - 1));
                 }
             }
         }
 
+
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
+
+
+        for (int index = 0; index < lettere.size(); index++) {
+            TextView t = new TextView(GiocoActivity.this);
+
+            if (index == 0) {
+                t.setText("" + lettere.get(index));
+            } else if (index == lettere.size() - 1) {
+                t.setText("" + lettere.get(index));
+            } else {
+                t.setText("_");
+            }
+
+            t.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT));
+            t.setWidth(90);
+            t.setId(index);
+            t.setTextSize(22);
+            t.setAllCaps(true);
+
+            layout1.addView(t);
+        }
+
+        for (int index = 1; index < lettere.size() - 1; index++) {
+            if (lettere.get(index).equals(lettere.get(0))) {
+                TextView t = (TextView) findViewById(index);
+                t.setText("" + lettere.get(0));
+            } else if (lettere.get(index).equals(lettere.get(lettere.size() - 1))) {
+                TextView t = (TextView) findViewById(index);
+                t.setText("" + lettere.get(lettere.size() - 1));
+            }
+        }
+
+        for(int ind=0;ind<lettereU.size();ind++){
+            TextView t= (TextView)findViewById(R.id.lettereU);
+            t.setText(t.getText()+"   "+lettereU.get(ind));
+
+            for(int index=1; index<lettere.size()-1;index++){
+
+                if((""+lettere.get(index)).equals(""+lettereU.get(ind))){
+                    layout1= (LinearLayout)findViewById(R.id.layout1);
+                    TextView t1= (TextView)layout1.getChildAt(index);
+                    t1.setText(""+lettereU.get(ind));
+                }
+            }
+
+            for (int index = 0; index < 26 - cont; index++) {
+                Button b = (Button) layout2.getChildAt(index);
+                if ((""+lettereU.get(ind)).equals("" + b.getText())) {
+                    layout2.removeViewAt(index);
+                    index--;
+                    cont++;
+                }
+            }
+        }
+
+        ImageView img=(ImageView)findViewById(R.id.imageView);
+        img.setBackgroundResource(getResources().getIdentifier("imp"+sbagli, "drawable",  getPackageName()));
+    }
+
+    private void nuovo(){
+        String json = null;
+        try {
+
+            InputStream is = this.getAssets().open("parole.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        ArrayList<String> parole = new ArrayList<>();
+
+        try {
+            JSONObject obj = new JSONObject(json);
+            JSONArray m_jArry = obj.getJSONArray("parole");
+
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                parole.add(jo_inside.getString("parola"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Random r = new Random();
+        int i = r.nextInt(1160 - 0) + 0;
+
+        parola = parole.get(i);
+
+        boolean flag = true;
+        int j = 0;
+
+        while (flag) {
+            try {
+                lettere.add((parole.get(i).charAt(j)));
+                j++;
+            } catch (Exception e) {
+                flag = false;
+            }
+        }
+
+        int cont = 0;
+
+        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
+        for (int index = 0; index < 26 - cont; index++) {
+            Button b = (Button) layout2.getChildAt(index);
+            if (("" + lettere.get(0)).equals("" + b.getText())) {
+                layout2.removeViewAt(index);
+                index--;
+                cont++;
+            } else if (!("" + lettere.get(0)).equals(("" + lettere.get(lettere.size() - 1)))) {
+                if (("" + lettere.get(lettere.size() - 1)).equals("" + b.getText())) {
+                    layout2.removeViewAt(index);
+                    index--;
+                    cont++;
+                }
+            }
+        }
+
+
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
+
+
+        for (int index = 0; index < lettere.size(); index++) {
+            TextView t = new TextView(GiocoActivity.this);
+
+            if (index == 0) {
+                t.setText("" + lettere.get(index));
+            } else if (index == lettere.size() - 1) {
+                t.setText("" + lettere.get(index));
+            } else {
+                t.setText("_");
+                lett++;
+            }
+
+            t.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT));
+            t.setWidth(90);
+            t.setId(index);
+            t.setTextSize(22);
+            t.setAllCaps(true);
+
+            layout1.addView(t);
+        }
+
+        for (int index = 1; index < lettere.size() - 1; index++) {
+            if (lettere.get(index).equals(lettere.get(0))) {
+                TextView t = (TextView) findViewById(index);
+                t.setText("" + lettere.get(0));
+                lett--;
+            } else if (lettere.get(index).equals(lettere.get(lettere.size() - 1))) {
+                TextView t = (TextView) findViewById(index);
+                t.setText("" + lettere.get(lettere.size() - 1));
+                lett--;
+            }
+        }
     }
 
     public void lettere(View view) {
@@ -279,7 +322,8 @@ public class GiocoActivity extends AppCompatActivity {
 
         if(lett==0){
             setContentView(R.layout.activity_risultato);
-            sbagli=0;
+            risultato=true;
+            risultato();
         }
         else if(sbagli==12){
             try {
@@ -288,16 +332,54 @@ public class GiocoActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             setContentView(R.layout.activity_risultato);
-            sbagli=0;
+            risultato=true;
+            risultato();
         }
     }
+
+    private void risultato(){
+        if(sbagli!=12){
+            TextView titolo=(TextView)findViewById(R.id.titolo);
+            titolo.setText("HAI INDOVINATO");
+        }
+        else{
+            TextView titolo=(TextView)findViewById(R.id.titolo);
+            titolo.setText("NON HAI INDOVINATO");
+        }
+
+        TextView parola=(TextView)findViewById(R.id.parola);
+        parola.setText("La parola da indovinare era: "+this.parola);
+
+        TextView tentativi=(TextView)findViewById(R.id.tentativi);
+        tentativi.setText("Hai utilizzato in totale "+lettereu.length()+" tentativi");
+    }
+
+    public void menu(View view){
+        Intent i = new Intent(GiocoActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void prossima(View view){
+        sbagli=0;
+        numParole++;
+        prossima=true;
+        risultato=false;
+        Activity a=GiocoActivity.this;
+        Intent i = new Intent(GiocoActivity.this, GiocoActivity.class);
+        i.putExtra("numParole",numParole);
+        startActivity(i);
+    }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt("lett",lett);
         outState.putInt("sbagli",sbagli);
+        outState.putInt("numParole",numParole);
         outState.putString("parola",parola);
         outState.putString("lettereU",lettereu);
+        outState.putBoolean("prossima",prossima);
+        outState.putBoolean("risultato",prossima);
         super.onSaveInstanceState(outState);
     }
 
@@ -307,8 +389,11 @@ public class GiocoActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = getSharedPreferences("prova",MODE_PRIVATE).edit();
         editor.putInt("lett",lett);
         editor.putInt("sbagli",sbagli);
+        editor.putInt("numParole",numParole);
         editor.putString("parola",parola);
         editor.putString("lettereU",lettereu);
+        editor.putBoolean("prossima",prossima);
+        editor.putBoolean("risultato",prossima);
         editor.apply();
     }
 }
--- app/src/main/java/com/example/studente/impiccato/MainActivity.java
+++ app/src/main/java/com/example/studente/impiccato/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.studente.impiccato;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +27,52 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, GiocoActivity.class);
+                i.putExtra("numParole",1);
                 startActivity(i);
             }
         });
+
+    }
+
+    public void setting(View v) {
+        setContentView(R.layout.activity_setting);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lingue, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    public void salva(View view){
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        if((String.valueOf(spinner.getSelectedItem())).equals("Italiano")){
+            Locale locale = new Locale("it");
+            Locale.setDefault(locale);
+
+            Resources res = this.getResources();
+            Configuration config = new Configuration(res.getConfiguration());
+            config.locale = locale;
+            res.updateConfiguration(config, res.getDisplayMetrics());
+        }
+        else{
+            Locale locale = new Locale("en");
+            Locale.setDefault(locale);
+
+            Resources res = this.getResources();
+            Configuration config = new Configuration(res.getConfiguration());
+            config.locale = locale;
+            res.updateConfiguration(config, res.getDisplayMetrics());
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
